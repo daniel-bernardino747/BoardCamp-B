e696d3bd65074a5f32fb48d205587c8e1022b5bd
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-12-07 18:38:58 -0300

    feat: express routes setup

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import routes from './routes/index.js'
+
+dotenv.config()
+
+const app = express()
+app.use(cors()).use(express.json()).use(routes)
+
+const port = process.env.PORT || 3333
+app.listen(port, () => {
+  console.log(`🌀 started server in door: ${port}`)
+})
