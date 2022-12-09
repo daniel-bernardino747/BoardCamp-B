cb9ce24489df1b04069514c20f0f4f79376a3245
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-12-09 17:43:25 -0300

    fix: error to PATCH customers

--- src/middlewares/customers.middlewares.ts
+++ src/middlewares/customers.middlewares.ts
@@ -56,12 +56,20 @@ export async function validateUsedCPF(
   next: NextFunction
 ) {
   const { validCustomer } = res.locals
+  const { id } = req.params
 
   try {
-    const searchCustomerInDatabase = await connection.query(
-      'SELECT * FROM customers WHERE cpf=$1',
-      [validCustomer.cpf]
-    )
+    const isToPut = req.route.methods.put
+
+    const query = isToPut
+      ? 'SELECT * FROM customers WHERE id<>$1 AND cpf=$2;'
+      : 'SELECT * FROM customers WHERE cpf=$1;'
+    const paramsQuery = isToPut
+      ? [Number(id), validCustomer.cpf]
+      : [validCustomer.cpf]
+
+    const searchCustomerInDatabase = await connection.query(query, paramsQuery)
+
     const existingCustomer = !!searchCustomerInDatabase.rows.length
     if (existingCustomer) return res.sendStatus(409)
 
